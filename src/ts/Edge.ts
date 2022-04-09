30f3996d77a15bcb3cd03e8be1c2ad57222ef6dc
adi <adityavikramsinha19@gmail.com>
2022-04-09 13:44:36 +0530

    added the Edge class

---
+++ src/ts/Edge.ts
@@ -0,0 +1,16 @@
+import Node from "./Node";
+
+export default class Edge<T> {
+    dest: Node<T>;
+    cost: number;
+
+    constructor(dest: Node<T>, cost: number) {
+        this.dest = dest;
+        this.cost = cost;
+    }
+    changeCost(_newCost:number):void{this.cost=_newCost;}
+    changeDest(_newDest:Node<T>):void{this.dest=_newDest;}
+    toString():string{
+        return '{dest:'+this.dest+', cost:'+this.cost+'}';
+    }
+}
