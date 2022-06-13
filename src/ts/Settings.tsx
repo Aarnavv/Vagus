7e163832e846ec4fc1636db65e6f43b362202c4e
aditya <adityavikramsinha19@gmail.com>
2022-06-13 12:31:29 +0530

    prepared backend for the settings.json in globalstate

--- src/ts/Settings.tsx
+++ src/ts/Settings.tsx
@@ -2,6 +2,7 @@ import * as React from "react";
 import * as ActionIcons from '../svgIcons/actionButtons';
 import '../css/settings.css';
 import { updateIDClass } from './Utility';
+import currentState from "./GlobalState";
 
 export default class Settings extends React.Component {
   static toggleDisplay = () => {
@@ -20,16 +21,18 @@ export default class Settings extends React.Component {
   static braceOpen = () => { return '{' }
   static braceClosed = () => { return '}' }
 
-  static renderKey = (keys: string[], values: string[]) => {
-    let keyJSX = []
-    for (let j = 0; j < keys.length; j++) {
+  static renderKey = (mapper : Map<string, string>): Array<JSX.Element> => {
+    let keyJSX : Array<JSX.Element> = [];
+    let i : number = 0;
+    mapper.forEach((value, key) => {
       keyJSX.push(
-        <div className="key-value" id={`key-value-${j}`} key={j}>
-          <div className="keys-div"><span className="blue" id={`key-${j}`}> {`"${keys[j]}"`}</span>: </div>
-          <div className="values-div"><span className="blue">"#<span className="blue" id={`value-${j}`} contentEditable="true" suppressContentEditableWarning={true}>{`${values[j]}`}</span>"</span>,</div>
+        <div className="key-value" id={`key-value-${i}`} key={i}>
+          <div className="keys-div"><span className="blue" id={`key-${i}`}> {`"${key}"`}</span>: </div>
+          <div className="values-div"><span className="blue">"#<span className="blue" id={`value-${i}`} contentEditable="true" suppressContentEditableWarning={true}>{`${value}`}</span>"</span>,</div>
         </div>
-      )
-    }
+      );
+      i++;
+    });
     return keyJSX;
   }
 
@@ -50,7 +53,7 @@ export default class Settings extends React.Component {
               <div className="braces"><p>{Settings.braceOpen()}</p></div>
               <div className="key-value-master-drop">
                 <div className="key-value-master">
-                  {Settings.renderKey(['CMD_BG', 'CMD_BORDER', 'PROJECT_BG', 'MOZ_SB_COLOR', 'SB_COLOR', 'SB_COLOR_HOVER', 'SB_COLOR_TRACK', 'FILE_HOVER', 'ALGO_FOLDER', 'NODE_FOLDER', 'MAZE_FOLDER', 'SPEED_FOLDER', 'LEGEND_FOLDER', 'HEX_COLOR', 'HEX_COLOR_HOVER', 'WALL_NODE_COLOR', 'PATH_NODE_COLOR1', 'PATH_NODE_COLOR2', 'PATH_NODE_COLOR3', 'PATH_NODE_COLOR4', 'PATH_NODE_COLOR5', 'VISITED_NODE_COLOR1', 'VISITED_NODE_COLOR2', 'VISITED_NODE_COLOR3', 'VISITED_NODE_COLOR11', 'VISITED_NODE_COLOR21', 'VISITED_NODE_COLOR31'], ['21252B', '434B57', '323844', '4b4e5578', 'ffffff42', 'ffffff60', '7f808200', '2C313A', 'D5756C', '67BBFF', '4CAF50', 'E5C07B', 'EF5350', '282C34', '1B8BCD', '484E5B', 'FFA500', 'FF9000', 'FF7B00', 'FF6702', 'FF5000', '175ab5', '1b8bcd', '25c8cf', 'BF4286', 'D15FDA', '85404F'])}
+                  {Settings.renderKey(currentState.cssVariables())}
                 </div>
               </div>
               <div className="braces"><p>{Settings.braceClosed()}</p></div>
